b84de41dc31a6f3c87412c781a1f194f4be7c109
manu <dr_manu71@hotmail.com>
2014-02-14 12:21:50 +0000

    modi

---
+++ practicaETs/src/practicaETs/Grados.java
@@ -0,0 +1,27 @@
+package practicaETs;
+
+public class Grados {
+	
+	private double gr;
+	// constructor 
+	 public Grados(double grados){
+		 this.gr = grados;
+	 }
+
+	public double getGrados() {
+		return gr;
+	}
+
+	public void setGrados(double grados) {
+		this.gr = grados;
+	}
+
+	 public double getCentigrado(){
+		return  (gr -32) * (5/9.0);
+		  
+	 }
+	 public double getFarenheit(){
+		 return ((9/5.0)* gr) +32;
+	 }
+	 
+}
